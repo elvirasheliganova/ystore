517dd0b291b8289f080f7fbd2c59c4726426b493
elivra <elvirashelyganova@gmail.com>
2025-05-02 22:40:41 +0200

    Root layout and Constants

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+
+
+const Homepage = () => {
+  return <  >
+    Yarn store</>;
+}
+ 
+export default Homepage;
---
+++ src/lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Yarn store';
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'Yarn store is a modern ecommerse platform built with Next.js';
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
