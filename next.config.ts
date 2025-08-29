d6c1442076647c90aca389bee056dd99a6cb5925
elivra <elvirashelyganova@gmail.com>
2025-08-29 16:30:58 +0200

    lint ignore while build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  eslint: {
+    // ✅ Don’t block production builds on ESLint errors
+    ignoreDuringBuilds: true,
+  },
+
  
   images: {
     remotePatterns: [
